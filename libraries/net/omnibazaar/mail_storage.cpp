fa093f8247c21d6318f97fd8d372f54b72727b54
AlexeyM <alexey.m@scopicsoftware.com>
2018-03-15 17:03:43 +0200

    Removed mail files extension

--- libraries/net/omnibazaar/mail_storage.cpp
+++ libraries/net/omnibazaar/mail_storage.cpp
@@ -6,7 +6,6 @@
 #include <fc/thread/scoped_lock.hpp>
 #include <fc/io/json.hpp>
 
-static const std::string TXT_EXTENSION(".txt");
 static const std::string DELIVERED_STR("delivered");
 static const std::string UNDELIVERED_STR("undelivered");
 // One folder is for storing mail that was sent but not yet flagged as received,
@@ -87,7 +86,7 @@ namespace omnibazaar {
 
         // Save to disk.
         fc::create_directories(_parent_dir / UNDELIVERED_STR);
-        fc::json::save_to_file(mail, _parent_dir / UNDELIVERED_STR / (mail.uuid + TXT_EXTENSION));
+        fc::json::save_to_file(mail, _parent_dir / UNDELIVERED_STR / mail.uuid);
 
         // Save to cache.
         _cache_by_uuid[mail.uuid] = mail_info(mail.recipient, false);
@@ -108,7 +107,7 @@ namespace omnibazaar {
         // Remove from disk.
         for(auto folder_info : MAIL_FOLDERS)
         {
-            const fc::path mail_path = _parent_dir / folder_info.first / (mail_uuid + TXT_EXTENSION);
+            const fc::path mail_path = _parent_dir / folder_info.first / mail_uuid;
             if(fc::exists(mail_path))
             {
                 fc::remove(mail_path);
@@ -162,7 +161,7 @@ namespace omnibazaar {
         std::pair<iter_type, iter_type> itrs = _cache_by_receiver.equal_range(receiver);
         while(itrs.first != itrs.second)
         {
-            const fc::path mail_path = _parent_dir / UNDELIVERED_STR / ((*itrs.first).second + TXT_EXTENSION);
+            const fc::path mail_path = _parent_dir / UNDELIVERED_STR / (*itrs.first).second;
             if(fc::exists(mail_path))
             {
                 const fc::variant var = fc::json::from_file(mail_path);
@@ -202,10 +201,10 @@ namespace omnibazaar {
         }
 
         // Move mail file to delivered folder.
-        const fc::path current_mail_path = _parent_dir / UNDELIVERED_STR / (mail_uuid + TXT_EXTENSION);
+        const fc::path current_mail_path = _parent_dir / UNDELIVERED_STR / mail_uuid;
         if(fc::exists(current_mail_path))
         {
-            const fc::path new_mail_path = _parent_dir / DELIVERED_STR / (mail_uuid + TXT_EXTENSION);
+            const fc::path new_mail_path = _parent_dir / DELIVERED_STR / mail_uuid;
             fc::rename(current_mail_path, new_mail_path);
         }
     }
