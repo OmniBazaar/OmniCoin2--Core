b945a6fad24993f300659b25de5422e7b8ebb7e3
AlexeyM <alexey.m@scopicsoftware.com>
2019-07-15 11:54:31 +0300

    Fixed build issue

--- libraries/plugins/elasticsearch/elasticsearch_plugin.cpp
+++ libraries/plugins/elasticsearch/elasticsearch_plugin.cpp
@@ -124,7 +124,7 @@ void elasticsearch_plugin_impl::update_account_histories( const signed_block& b 
       if( op.op.which() == operation::tag< account_create_operation >::value )
          impacted.insert( op.result.get<object_id_type>() );
       else
-         graphene::app::operation_get_impacted_accounts( op.op, impacted );
+         graphene::app::operation_get_impacted_accounts( db, op.op, impacted );
 
       for( auto& a : other )
          for( auto& item : a.account_auths )
